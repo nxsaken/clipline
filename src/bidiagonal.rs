4f99c0fe407bbd571acde75fd30b40a9ead34338
Nurzhan Sakén <nurzhan.sakenov@gmail.com>
2025-05-05 19:29:21 +0400

    feat: add `Bidiagonal` for optimized reverse iteration for diagonals
    Signed-off-by: Nurzhan Sakén <nurzhan.sakenov@gmail.com>

--- src/bidiagonal.rs
+++ src/bidiagonal.rs
@@ -1,33 +1,49 @@
 use crate::math::{ops, CxC, SxS, C, S, U};
 
 /// An iterator over the rasterized points of a half-open diagonal line segment.
+///
+/// This is a variant of the [`Diagonal`] iterator optimized
+/// for [`DoubleEndedIterator`] by storing both end coordinates.
+///
+/// If you do not need double-ended iteration, prefer [`Diagonal`].
+///
+/// [`Diagonal`]: crate::Diagonal
 #[derive(Clone, PartialEq, Eq, Hash, Debug)]
-pub struct Diagonal {
-    /// The start coordinate along the `x` axis.
+pub struct Bidiagonal {
+    /// The current start coordinate along the `x` axis.
     x0: C,
-    /// The start coordinate along the `y` axis.
+    /// The current start coordinate along the `y` axis.
     y0: C,
+    /// The current end coordinate along the `x` axis.
+    x1: C,
+    /// The current end coordinate along the `y` axis.
+    y1: C,
     /// The step sign along the `x` axis.
     sx: S,
     /// The step sign along the `y` axis.
     sy: S,
-    /// The end coordinate along the `x` axis.
-    x1: C,
 }
 
-impl Diagonal {
-    /// Constructs a [`Diagonal`] iterator from its internal parts.
+impl Bidiagonal {
+    /// Constructs a [`Bidiagonal`] iterator from its internal parts.
     ///
     /// # Safety
     ///
-    /// `sx` must match the direction from `x0` to `x1`.
+    /// 1. `|x0 - x1|` must be equal to `|y0 - y1|`.
+    /// 2. `sx` must match the direction from `x0` to `x1`.
+    /// 3. `sy` must match the direction from `y0` to `y1`.
     #[inline]
     #[must_use]
-    pub const unsafe fn new_unchecked((x0, y0): CxC, (sx, sy): SxS, x1: C) -> Self {
-        Self { x0, y0, sx, sy, x1 }
+    pub const unsafe fn new_unchecked((x0, y0): CxC, (x1, y1): CxC, (sx, sy): SxS) -> Self {
+        debug_assert!(x0.abs_diff(x1) == y0.abs_diff(y1));
+        debug_assert!((x0 <= x1) == matches!(sx, S::P));
+        debug_assert!((x1 < x0) == matches!(sx, S::N));
+        debug_assert!((y0 <= y1) == matches!(sy, S::P));
+        debug_assert!((y1 < y0) == matches!(sy, S::N));
+        Self { x0, y0, x1, y1, sx, sy }
     }
 
-    /// Returns a [`Diagonal`] iterator over a half-open line segment
+    /// Returns a [`Bidiagonal`] iterator over a half-open line segment
     /// if the line segment is diagonal, otherwise returns [`None`].
     #[inline]
     #[must_use]
@@ -39,13 +55,16 @@ impl Diagonal {
             return None;
         }
 
-        // SAFETY: sx matches the direction from x0 to x1.
-        let this = unsafe { Self::new_unchecked((x0, y0), (sx, sy), x1) };
+        // SAFETY:
+        // 1. |x0 - x1| is equal to |y0 - y1|.
+        // 2. sx matches the direction from x0 to x1.
+        // 3. sy matches the direction from y0 to y1.
+        let this = unsafe { Self::new_unchecked((x0, y0), (x1, y1), (sx, sy)) };
         Some(this)
     }
 }
 
-impl Diagonal {
+impl Bidiagonal {
     /// Returns `true` if the iterator has terminated.
     #[inline]
     #[must_use]
@@ -87,7 +106,7 @@ impl Diagonal {
     }
 }
 
-impl Iterator for Diagonal {
+impl Iterator for Bidiagonal {
     type Item = CxC;
 
     #[inline]
@@ -103,11 +122,11 @@ impl Iterator for Diagonal {
     }
 }
 
-impl core::iter::FusedIterator for Diagonal {}
+impl core::iter::FusedIterator for Bidiagonal {}
 
-impl ExactSizeIterator for Diagonal {}
+impl ExactSizeIterator for Bidiagonal {}
 
-impl Diagonal {
+impl Bidiagonal {
     /// Returns the point immediately before the end of the iterator.
     /// This does not advance the iterator.
     ///
@@ -115,11 +134,8 @@ impl Diagonal {
     ///
     /// # Performance
     ///
-    /// This method performs non-trivial arithmetic to compute the last point.
-    /// Use sparingly in performance-critical code. Avoid pairing this with
-    /// [`Self::pop_tail`], as it will redo that work.
-    ///
-    /// See [`Bidiagonal`](crate::Bidiagonal) for an optimized alternative.
+    /// This method performs trivial arithmetic to compute the last point.
+    /// Avoid pairing this with [`Self::pop_tail`], as it will redo that work.
     #[inline]
     #[must_use]
     pub const fn tail(&self) -> Option<CxC> {
@@ -127,11 +143,7 @@ impl Diagonal {
             return None;
         }
         let x1 = self.x1.wrapping_sub(self.sx as C);
-        let dx = x1.abs_diff(self.x0);
-        let y1 = match self.sy {
-            S::P => self.y0.wrapping_add_unsigned(dx),
-            S::N => self.y0.wrapping_sub_unsigned(dx),
-        };
+        let y1 = self.y1.wrapping_sub(self.sy as C);
         Some((x1, y1))
     }
 
@@ -139,23 +151,17 @@ impl Diagonal {
     /// This advances the iterator backwards.
     ///
     /// Returns [`None`] if the iterator has terminated.
-    ///
-    /// # Performance
-    ///
-    /// This method performs non-trivial arithmetic to compute the last point.
-    /// Use sparingly in performance-critical code.
-    ///
-    /// See [`Bidiagonal`](crate::Bidiagonal) for an optimized alternative.
     #[inline]
     #[must_use]
     pub const fn pop_tail(&mut self) -> Option<CxC> {
         let Some((x1, y1)) = self.tail() else { return None };
         self.x1 = x1;
+        self.y1 = y1;
         Some((x1, y1))
     }
 }
 
-impl DoubleEndedIterator for Diagonal {
+impl DoubleEndedIterator for Bidiagonal {
     #[inline]
     fn next_back(&mut self) -> Option<Self::Item> {
         self.pop_tail()
--- src/lib.rs
+++ src/lib.rs
@@ -15,12 +15,14 @@
 #![warn(clippy::pedantic)]
 
 mod axis;
+mod bidiagonal;
 mod bresenham;
 mod bresenham_case;
 mod diagonal;
 mod math;
 
 pub use axis::{Axis, Axis0, Axis1};
+pub use bidiagonal::Bidiagonal;
 pub use bresenham::Bresenham;
 pub use bresenham_case::{BresenhamCase, BresenhamFast, BresenhamSlow};
 pub use diagonal::Diagonal;
