01948fb99c16e78dcefe36e2ba629452736f8f41
Nurzhan Sakén <nurzhan.sakenov@gmail.com>
2025-07-15 15:20:26 +0400

    feat(clip): stubs for `line_b` and `line_d`
    Signed-off-by: Nurzhan Sakén <nurzhan.sakenov@gmail.com>

--- src/clip/line_b.rs
+++ src/clip/line_b.rs
@@ -1,27 +1,105 @@
-use crate::clip::ClipV;
+use crate::clip::{Clip, ClipV};
 use crate::line_b::{LineB, LineBu, LineBx, LineBy};
 use crate::math::Coord;
 
-impl<C: Coord> ClipV<C> {
-    pub const fn line_bu<const YX: bool>(
-        &self,
-        x0: C,
-        y0: C,
-        x1: C,
-        y1: C,
-    ) -> Option<LineBu<YX, C>> {
-        None
-    }
+struct Impl<C: Coord>(C);
 
-    pub const fn line_bx(&self, x0: C, y0: C, x1: C, y1: C) -> Option<LineBx<C>> {
-        None
-    }
+macro_rules! clip_line_b {
+    ($U:ty | $I:ty) => {
+        clip_line_b!(common $U, $U);
+        clip_line_b!(common $I, $U);
+        clip_line_b!(signed $I, $U);
+    };
+    (common $UI:ty, $U:ty) => {
+        impl Impl<$UI> {
+            const unsafe fn reject_bbox_closed<const FX: bool, const FY: bool>(
+                x_min: $UI,
+                y_min: $UI,
+                x_max: $UI,
+                y_max: $UI,
+                x0: $UI,
+                y0: $UI,
+                x1: $UI,
+                y1: $UI,
+            ) -> bool {
+                !FX && (x1 < x_min || x_max < x0)
+                    || FX && (x0 < x_min || x_max < x1)
+                    || !FY && (y1 < y_min || y_max < y0)
+                    || FY && (y0 < y_min || y_max < y1)
+            }
+        }
 
-    pub const fn line_by(&self, x0: C, y0: C, x1: C, y1: C) -> Option<LineBy<C>> {
-        None
-    }
+        impl Clip<$UI> {
+            pub const fn line_bu<const YX: bool>(&self, x0: $UI, y0: $UI, x1: $UI, y1: $UI) -> Option<LineBu<YX, $UI>> {
+                None
+            }
 
-    pub const fn line_b(&self, x0: C, y0: C, x1: C, y1: C) -> Option<LineB<C>> {
-        None
-    }
+            pub const fn line_bx(&self, x0: $UI, y0: $UI, x1: $UI, y1: $UI) -> Option<LineBx<$UI>> {
+                None
+            }
+            pub const fn line_by(&self, x0: $UI, y0: $UI, x1: $UI, y1: $UI) -> Option<LineBy<$UI>> {
+                None
+            }
+
+            pub const fn line_b(&self, x0: $UI, y0: $UI, x1: $UI, y1: $UI) -> Option<LineB<$UI>> {
+                None
+            }
+        }
+
+        impl ClipV<$UI> {
+            pub const fn line_bu<const YX: bool>(&self, x0: $UI, y0: $UI, x1: $UI, y1: $UI) -> Option<LineBu<YX, $UI>> {
+                None
+            }
+
+            pub const fn line_bx(&self, x0: $UI, y0: $UI, x1: $UI, y1: $UI) -> Option<LineBx<$UI>> {
+                None
+            }
+            pub const fn line_by(&self, x0: $UI, y0: $UI, x1: $UI, y1: $UI) -> Option<LineBy<$UI>> {
+                None
+            }
+
+            pub const fn line_b(&self, x0: $UI, y0: $UI, x1: $UI, y1: $UI) -> Option<LineB<$UI>> {
+                None
+            }
+
+            pub const fn line_bu_o<const YX: bool>(&self, x0: $UI, y0: $UI, x1: $UI, y1: $UI) -> Option<LineBu<YX, $U>> {
+                None
+            }
+
+            pub const fn line_bx_o(&self, x0: $UI, y0: $UI, x1: $UI, y1: $UI) -> Option<LineBx<$U>> {
+                None
+            }
+            pub const fn line_by_o(&self, x0: $UI, y0: $UI, x1: $UI, y1: $UI) -> Option<LineBy<$U>> {
+                None
+            }
+
+            pub const fn line_b_o(&self, x0: $UI, y0: $UI, x1: $UI, y1: $UI) -> Option<LineB<$U>> {
+                None
+            }
+        }
+    };
+    (signed $I:ty, $U:ty) => {
+        impl Clip<$I> {
+            pub const fn line_bu_o<const YX: bool>(&self, x0: $I, y0: $I, x1: $I, y1: $I) -> Option<LineBu<YX, $U>> {
+                None
+            }
+
+            pub const fn line_bx_o(&self, x0: $I, y0: $I, x1: $I, y1: $I) -> Option<LineBx<$U>> {
+                None
+            }
+            pub const fn line_by_o(&self, x0: $I, y0: $I, x1: $I, y1: $I) -> Option<LineBy<$U>> {
+                None
+            }
+
+            pub const fn line_b_o(&self, x0: $I, y0: $I, x1: $I, y1: $I) -> Option<LineB<$U>> {
+                None
+            }
+        }
+    };
 }
+
+clip_line_b!(u8 | i8);
+clip_line_b!(u16 | i16);
+clip_line_b!(u32 | i32);
+clip_line_b!(u64 | i64);
+clip_line_b!(usize | isize);
--- src/clip/line_d.rs
+++ src/clip/line_d.rs
@@ -1,64 +1,78 @@
 use crate::Clip;
 use crate::clip::ClipV;
 use crate::line_d::{LineD, LineD2};
+use crate::math::Coord;
 
-impl Clip<i16> {
-    const unsafe fn reject_bbox_half_open<const FX: bool, const FY: bool>(
-        &self,
-        x0: i16,
-        y0: i16,
-        x1: i16,
-        y1: i16,
-    ) -> bool {
-        !FX && (x1 <= 0 || self.x_max < x0)
-            || FX && (x0 < 0 || self.x_max <= x1)
-            || !FY && (y1 <= 0 || self.y_max < y0)
-            || FY && (y0 < 0 || self.y_max <= y1)
-    }
+struct Impl<C: Coord>(C);
+
+macro_rules! clip_line_d {
+    ($Cu:ty | $Ci:ty) => {
+        clip_line_d!(common $Cu, $Cu);
+        clip_line_d!(common $Ci, $Cu);
+        clip_line_d!(signed $Ci, $Cu);
+    };
+    (common $C:ty, $Cu:ty) => {
+        impl Impl<$C> {
+            const unsafe fn reject_bbox_half_open<const FX: bool, const FY: bool>(
+                x_min: $C,
+                y_min: $C,
+                x_max: $C,
+                y_max: $C,
+                x0: $C,
+                y0: $C,
+                x1: $C,
+                y1: $C,
+            ) -> bool {
+                !FX && (x1 <= x_min || x_max < x0)
+                    || FX && (x0 < x_min || x_max <= x1)
+                    || !FY && (y1 <= y_min || y_max < y0)
+                    || FY && (y0 < y_min || y_max <= y1)
+            }
+        }
+
+        impl Clip<$C> {
+            pub const fn line_d(&self, x0: $C, y0: $C, x1: $C, y1: $C) -> Option<LineD<$C>> {
+                None
+            }
+
+            pub const fn line_d2(&self, x0: $C, y0: $C, x1: $C, y1: $C) -> Option<LineD2<$C>> {
+                None
+            }
+        }
+
+        impl ClipV<$C> {
+            pub const fn line_d(&self, x0: $C, y0: $C, x1: $C, y1: $C) -> Option<LineD<$C>> {
+                None
+            }
+
+            pub const fn line_d2(&self, x0: $C, y0: $C, x1: $C, y1: $C) -> Option<LineD2<$C>> {
+                None
+            }
+
+            pub const fn line_d_o(&self, x0: $C, y0: $C, x1: $C, y1: $C) -> Option<LineD<$Cu>> {
+                None
+            }
+
+            pub const fn line_d2_o(&self, x0: $C, y0: $C, x1: $C, y1: $C) -> Option<LineD2<$Cu>> {
+                None
+            }
+        }
+    };
+    (signed $C:ty, $Cu:ty) => {
+        impl Clip<$C> {
+            pub const fn line_d_o(&self, x0: $C, y0: $C, x1: $C, y1: $C) -> Option<LineD<$Cu>> {
+                None
+            }
+
+            pub const fn line_d2_o(&self, x0: $C, y0: $C, x1: $C, y1: $C) -> Option<LineD2<$Cu>> {
+                None
+            }
+        }
+    };
 }
 
-impl Clip<u16> {
-    pub const fn line_d(&self, x0: u16, y0: u16, x1: u16, y1: u16) -> Option<LineD<u16>> {
-        None
-    }
-
-    pub const fn line_d2(&self, x0: u16, y0: u16, x1: u16, y1: u16) -> Option<LineD2<u16>> {
-        None
-    }
-}
-
-impl Clip<i16> {
-    pub const fn line_d(&self, x0: i16, y0: i16, x1: i16, y1: i16) -> Option<LineD<i16>> {
-        None
-    }
-
-    pub const fn line_d2(&self, x0: i16, y0: i16, x1: i16, y1: i16) -> Option<LineD2<i16>> {
-        None
-    }
-
-    pub const fn line_d_o(&self, x0: i16, y0: i16, x1: i16, y1: i16) -> Option<LineD<u16>> {
-        None
-    }
-
-    pub const fn line_d2_o(&self, x0: i16, y0: i16, x1: i16, y1: i16) -> Option<LineD2<u16>> {
-        None
-    }
-}
-
-impl ClipV<i16> {
-    pub const fn line_d(&self, x0: i16, y0: i16, x1: i16, y1: i16) -> Option<LineD<i16>> {
-        None
-    }
-
-    pub const fn line_d2(&self, x0: i16, y0: i16, x1: i16, y1: i16) -> Option<LineD2<i16>> {
-        None
-    }
-
-    pub const fn line_d_o(&self, x0: i16, y0: i16, x1: i16, y1: i16) -> Option<LineD<u16>> {
-        None
-    }
-
-    pub const fn line_d2_o(&self, x0: i16, y0: i16, x1: i16, y1: i16) -> Option<LineD2<u16>> {
-        None
-    }
-}
+clip_line_d!(u8 | i8);
+clip_line_d!(u16 | i16);
+clip_line_d!(u32 | i32);
+clip_line_d!(u64 | i64);
+clip_line_d!(usize | isize);
